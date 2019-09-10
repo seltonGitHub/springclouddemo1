3ed9b574f1fc3745470caee4dd695c84fcfae439
Selton <seltonzyf@outlook.com>
2019-09-10 17:15:25 +0800

    enhancement #3 init springcloud eureka provider server.

---
+++ eurekaserverprovicer1/src/main/java/com/itszt/eurekaserverprovider1/EurekaserverProviderApplication.java
@@ -0,0 +1,16 @@
+package com.itszt.eurekaserverprovider1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaserverProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverProviderApplication.class, args);
+	}
+
+}
