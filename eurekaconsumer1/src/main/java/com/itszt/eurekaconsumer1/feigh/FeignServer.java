80763e298236b47c0c044d3585dc97b401297a97
Selton <seltonzyf@outlook.com>
2019-09-11 17:26:40 +0800

    enhancement #? 断路器.

--- eurekaconsumer1/src/main/java/com/itszt/eurekaconsumer1/feigh/FeignServer.java
+++ eurekaconsumer1/src/main/java/com/itszt/eurekaconsumer1/feigh/FeignServer.java
@@ -1,9 +1,10 @@
 package com.itszt.eurekaconsumer1.feigh;
 
+import com.itszt.eurekaconsumer1.feigh.hystrix.FeighServerImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("SPRING-CLOUD-EUREKA-PROVIDER")
+@FeignClient(value = "SPRING-CLOUD-EUREKA-PROVIDER", fallback = FeighServerImpl.class)
 public interface FeignServer {
 
     @GetMapping("/user")
---
+++ eurekaconsumer1/src/main/java/com/itszt/eurekaconsumer1/feigh/hystrix/FeighServerImpl.java
@@ -0,0 +1,12 @@
+package com.itszt.eurekaconsumer1.feigh.hystrix;
+
+import com.itszt.eurekaconsumer1.feigh.FeignServer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeighServerImpl implements FeignServer {
+    @Override
+    public String getUser() {
+        return "断路器 --> work";
+    }
+}
