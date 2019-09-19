7bca7376271118ecf0e2b1209c89a4ebdd87e751
JZC <a403239297@163.com>
2019-09-19 20:01:36 +0800

    添加了熔断器大屏

--- eurekaserverconsumer/src/main/java/com/springc/EurekaserverConsumerApplication.java
+++ eurekaserverconsumer/src/main/java/com/springc/EurekaserverConsumerApplication.java
@@ -1,11 +1,15 @@
 package com.springc;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import com.springc.feign.FeignServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +17,20 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaserverConsumerApplication {
+
+	@Bean
+	public ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet =new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean=new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+
+		return registrationBean;
+	}
 
 	@Bean
 	@LoadBalanced
