00258544284a0f3b38c4201bfdf773fa0b55e6ae
JZC <a403239297@163.com>
2019-09-26 11:43:38 +0800

    添加了服务网关zuul，修改了从生产者调用服务，而是改为经过zuul分配

--- eurekaserverconsumer/src/main/java/com/springc/feign/FeignServer.java
+++ eurekaserverconsumer/src/main/java/com/springc/feign/FeignServer.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  * Created by JZC on 2019/9/17.
  */
-@FeignClient(value = "SPRING-CLOUD--PROVICER",fallback = FeignServerImpl.class)
+@FeignClient(value = "SPRING-CLOUD--ZUUL",fallback = FeignServerImpl.class)
 public interface FeignServer {
 
-    @GetMapping("/user")
+    @GetMapping("/provider/user")
     public String getUser();
 }
